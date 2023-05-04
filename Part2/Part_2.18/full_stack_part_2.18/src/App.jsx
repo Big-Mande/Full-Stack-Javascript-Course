d38d63e58044bcf9c3a0a4262d9d086b501f77d8
Christian Hirales <chirales97@gmail.com>
2023-05-03 23:42:15 -0700

    added more detailed information for a country

--- Part2/Part_2.18/full_stack_part_2.18/src/App.jsx
+++ Part2/Part_2.18/full_stack_part_2.18/src/App.jsx
@@ -15,11 +15,26 @@ const filterCountry = () => {
         const countries = countryNames
         ? countryNames.filter((name) => name.toLowerCase().includes(newCountry.toLowerCase()) )
         : countryNames
-        if (countries.length > 10) return ['Too many countires, narrow down your search'];
+        if (countries.length > 9) return ['Too many countires, narrow down your search'];
         return countries;
  }
 
 const filteredNames = filterCountry();
+
+const countryData = getSingleCountryData();
+
+const getSingleCountryData = () => {
+    countriesAPI.getData()
+    .then( response => {
+        for(let i = 0; i < response.length; i++){
+            if(response.data[i].name.common.toLowerCase() === filteredNames[0]){
+                return response.data[i]
+            }
+        }
+    })
+    .catch( (error) => console.error(`Could not load in data for ${filteredNames[0]}`));
+
+};
 
 // Initial data fetching
 useEffect( () => { 
@@ -38,6 +53,10 @@ useEffect( () => {
       <div>
       <ShowCountries newCountry={newCountry} handleChange={handleChange}/>
       <Countries countryNames={countryNames} filteredNames={filteredNames} />
+      {  filteredNames.length === 1
+         ?  <ShowCountryData countryData={countryData}/>
+         :  null
+      }
       </div>
     
   );
@@ -66,6 +85,30 @@ const Countries = ({countryNames, filteredNames}) =>{
             </ul>
         </div>
     );
+};
+
+const ShowCountryData = ({countryData}) => {
+   return(
+       <div>
+            <h3>{countryData.name.common}</h3>
+            <p>capital: {countryData.capital[0]}</p>
+            <p>area : {countryData.area}</p>
+
+            <br><br>
+
+            <p><b>Languages:</b></p>
+
+            <ul>
+                {countryData.languages.map( language => (
+                   <li key={countryData.name.common}>
+                        {language}
+                   </li>  
+                ))}            
+            </ul>
+
+            <img src={countryData.flags.svg} alt="flag here :D" />
+       </div>
+   ); 
 };
 
 export default App
