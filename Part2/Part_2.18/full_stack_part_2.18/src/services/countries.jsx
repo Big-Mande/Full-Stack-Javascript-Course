c064559d6e09185cb72cc918201d1b7021eca1d0
Christian Hirales <chirales97@gmail.com>
2023-05-04 19:20:18 -0700

    added updated functions for axios to make API calls

--- Part2/Part_2.18/full_stack_part_2.18/src/services/countries.jsx
+++ Part2/Part_2.18/full_stack_part_2.18/src/services/countries.jsx
@@ -1,6 +1,25 @@
 import axios from 'axios'
 const baseURL = 'https://restcountries.com/v3.1/all'
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?'
+const iconURL = 'https://openweathermap.org/img/wn/'
+const size = '2x.png'
+const apiKey = import.meta.env.VITE_WEATHER_APP_API_KEY
 
+// get all country data
 const getData = () => axios.get(baseURL);
 
-export default { getData }
+//takes in a country object and returns a promise that will give all weatherData for given capital
+const getWeatherData = (country) =>{ 
+                                        const requestURL=`${weatherURL}q=${country.capital[0]}&APPID=${apiKey}&units=imperial`; 
+                                        console.log(requestURL)
+                                       return axios.get(requestURL)
+                                        .then(response => response.data)
+                                        .catch(error => console.error('oops error fetching data'))
+};
+
+// takes in a countries weatherData and returns the promise that will generate the weather icon.
+const getWeatherIcons = (country) => axios.get(`${iconURL}${country.weather.icon}@${size}`);
+
+export default { getData, getWeatherData, getWeatherIcons }
+
+export {iconURL, size}
